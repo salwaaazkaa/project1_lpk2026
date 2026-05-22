73a877e37c49095acbaa0373b76c5d68523ced03
salwaaazkaa <azkasabanasalwa@gmail.com>
2026-05-22 14:31:10 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,3 @@ a soft return is used for the next line.
 Two (or more) newline characters in a row will result in a hard return.
 '''
 st.markdown(multi)
-import streamlit as st
-
-st.text("This is text\n[and more text](that's not a Markdown link).")
